366ccb2d08e8fb728ca66dfc3580316cf24d7285
Gustavo <gugasc7@gmail.com>
2023-07-24 21:53:33 -0300

    teste 123

--- console.js
+++ console.js
@@ -1,7 +1,11 @@
 //log -> registro
 
-const minhaVar = true;
+// const minhaVar = true;
 
-console.log(245)
-console.log("eu sou o Gustavo ;)")
-console.log(minhaVar)
+// console.log(245)
+// console.log("eu sou o Gustavo ;)")
+// console.log(minhaVar)
+
+//tratamento de erro!
+
+console.error('deu erro!')
